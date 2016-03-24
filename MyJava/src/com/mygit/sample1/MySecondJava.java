3be3f4ac82d50b7280ccb52674b05e0dbf9327fc
pardha74 <pvasamsetty@mitsind.com>
2016-03-23 22:44:08 -0700

    ss

--- MyJava/src/com/mygit/sample1/MySecondJava.java
+++ MyJava/src/com/mygit/sample1/MySecondJava.java
@@ -2,7 +2,7 @@ package com.mygit.sample1;
 
 public class MySecondJava {
  public String hello(){
-	return "Hellow how are you? xxxxxxxxxxxxxxxx";
+	return "Hellow how are you? xxxxxxxxxxxxxxxx Making changes in Pardha";
 	 
  }
 }
