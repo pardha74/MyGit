8a20de161af2aeadd92154d6ba20a56f933dd848
pardha74 <pvasamsetty@mitsind.com>
2016-03-23 23:22:36 -0700

    Second Java zzz

--- MyJava/src/com/mygit/sample1/MySecondJava.java
+++ MyJava/src/com/mygit/sample1/MySecondJava.java
@@ -2,11 +2,9 @@ package com.mygit.sample1;
 
 public class MySecondJava {
  public String hello(){
-<<<<<<< HEAD
-	return "Hellow how are you? xxxxxxxxxxxxxxxx  This is DEV";
-=======
-	return "Hellow how are you? xxxxxxxxxxxxxxxx Making changes in Pardhax";
->>>>>>> refs/remotes/origin/Pardha
+
+	return "Hellow how are you? xxxxxxxxxxxxxxxx Making changes in PardhaxZZZZ";
+
 	 
  }
 }
